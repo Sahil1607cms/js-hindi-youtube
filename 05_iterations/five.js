47f1b21dff1e5f3a78b9295c678e48c2341a20b9
Sahil1607cms <149762467+Sahil1607cms@users.noreply.github.com>
2024-12-15 23:54:57 +0530

    Update five.js

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,12 +1,14 @@
 const coding = ["js", "ruby", "java", "python", "cpp"]
 
-// coding.forEach( function (val){
-//     console.log(val);
-// } )
+coding.forEach( function (val){
+    console.log(val);
+} )
 
-// coding.forEach( (item) => {
-//     console.log(item);
-// } )
+ // OR WE CAN USE 
+
+coding.forEach( (item) => {
+    console.log(item);
+} )
 
 // function printMe(item){
 //     console.log(item);
