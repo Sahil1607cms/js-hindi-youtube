aa4c0a7633fd3c343ff06a5c216c1ece5a517063
Sahil1607cms <149762467+Sahil1607cms@users.noreply.github.com>
2024-12-15 23:51:02 +0530

    Update four.js

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -5,12 +5,14 @@ const myObject = {
     swift: "swift by apple"
 }
 
+//we use for....in for objects 
 for (const key in myObject) {
     //console.log(`${key} shortcut is for ${myObject[key]}`);
 }
 
 const programming = ["js", "rb", "py", "java", "cpp"]
 
+//we use for...in for arrays 
 for (const key in programming) {
     //console.log(programming[key]);
 }
@@ -21,6 +23,7 @@ for (const key in programming) {
 // map.set('Fr', "France")
 // map.set('IN', "India")
 
+//this will throw error because we cant iterate through maps using for...in maps
 // for (const key in map) {
 //     console.log(key);
 // }
