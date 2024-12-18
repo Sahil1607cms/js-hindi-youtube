4df28d87b490ab79d5eb3c8924a7e08e4f231315
Sahil1607cms <149762467+Sahil1607cms@users.noreply.github.com>
2024-12-18 22:32:11 +0530

    Update nine.js

--- 05_iterations/nine.js
+++ 05_iterations/nine.js
@@ -1,15 +1,15 @@
 const myNums = [1, 2, 3]
 
-// const myTotal = myNums.reduce(function (acc, currval) {
-//     console.log(`acc: ${acc} and currval: ${currval}`);
-//     return acc + currval
-// }, 0)
+//concept used in shopping cart
+const myTotal = myNums.reduce(function (accumulator, currval) {
+    console.log(`acc: ${acc} and currval: ${currval}`);
+    return acc + currval
+}, 0) //accumulator will store 0 in the first iteration only for the first time, it will then be updated and keep on changing 
 
 const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
-
 console.log(myTotal);
 
-
+//array of objects
 const shoppingCart = [
     {
         itemName: "js course",
