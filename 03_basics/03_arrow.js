c92c3ad5f482f9cf3f9e1b29c280c3fef3ecb124
Sahil1607cms <149762467+Sahil1607cms@users.noreply.github.com>
2024-12-18 20:57:27 +0530

    Update 03_arrow.js

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -3,6 +3,7 @@ const user = {
     price: 999,
 
     welcomeMessage: function() {
+        //always refer to current context using this
         console.log(`${this.username} , welcome to website`);
         console.log(this);
     }
@@ -11,38 +12,40 @@ const user = {
 
 // user.welcomeMessage()
 // user.username = "sam"
-// user.welcomeMessage()
+// user.welcomeMessage() this will change the name to sam from hitesh, context is changed 
 
-// console.log(this);
+// console.log(this); output: {}(empty object) in nodejs, and window(global object) methods in browser  
 
 // function chai(){
 //     let username = "hitesh"
-//     console.log(this.username);
+//     console.log(this.username); prints undefined, because this is used in object only not in functions mainly 
 // }
 
 // chai()
 
 // const chai = function () {
 //     let username = "hitesh"
-//     console.log(this.username);
+//     console.log(this.username); also undefined same concept as above
 // }
 
+//THERE IS NO THIS IN ARROW FUNCTION
 const chai =  () => {
     let username = "hitesh"
-    console.log(this);
+    console.log(this); //undefined 
 }
 
 
 // chai()
 
 // const addTwo = (num1, num2) => {
-//     return num1 + num2
+//     return num1 + num2 //using curly braces then write return
 // }
 
-// const addTwo = (num1, num2) =>  num1 + num2
+// const addTwo = (num1, num2) =>  num1 + num2   // this is implicit return, no need to write return since no curly braces are there 
 
 // const addTwo = (num1, num2) => ( num1 + num2 )
 
+//objects need to be within curly braces
 const addTwo = (num1, num2) => ({username: "hitesh"})
 
 
