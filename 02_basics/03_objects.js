2d8db553a93d633e494e796422521daaac85736e
Sahil1607cms <149762467+Sahil1607cms@users.noreply.github.com>
2024-12-18 18:29:51 +0530

    Update 03_objects.js

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -9,6 +9,7 @@ const mySym = Symbol("key1")
 const JsUser = {
     name: "Hitesh",
     "full name": "Hitesh Choudhary",
+    //only way of accessing symbols in object is using square brackets 
     [mySym]: "mykey1",
     age: 18,
     location: "Jaipur",
@@ -20,17 +21,21 @@ const JsUser = {
 // console.log(JsUser.email)
 // console.log(JsUser["email"])
 // console.log(JsUser["full name"])
+//access it using square bracket only 
 // console.log(JsUser[mySym])
 
 JsUser.email = "hitesh@chatgpt.com"
 // Object.freeze(JsUser)
+//after freezing no changes will be made to the object, also no error will be shown if we try to change the value after freezing 
 JsUser.email = "hitesh@microsoft.com"
 // console.log(JsUser);
 
+//this greeting will act as variable and it will have this function 
 JsUser.greeting = function(){
     console.log("Hello JS user");
 }
 JsUser.greetingTwo = function(){
+    //referencing to same object 
     console.log(`Hello JS user, ${this.name}`);
 }
 
