78a8637614373827499aee78f0d2948024da8407
Sahil1607cms <149762467+Sahil1607cms@users.noreply.github.com>
2024-12-18 20:30:37 +0530

    Update 04_objects.js

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -9,6 +9,7 @@ tinderUser.isLoggedIn = false
 
 const regularUser = {
     email: "some@gmail.com",
+    //nested object
     fullname: {
         userfullname: {
             firstname: "hitesh",
@@ -23,13 +24,13 @@ const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
-// const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// const obj3 = { obj1, obj2 } this will not give right format it will concatenate obj1 and obj2 just after one another
+// const obj3 = Object.assign({}, obj1, obj2, obj4) this is used to concatinate objects 
 
-const obj3 = {...obj1, ...obj2}
+const obj3 = {...obj1, ...obj2} //latest and best format to concatenate 
 // console.log(obj3);
 
-
+//when we get data from database we get array of objects
 const users = [
     {
         id: 1,
@@ -48,13 +49,16 @@ const users = [
 users[1].email
 // console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+//VVVVVIMPPPPP
+// console.log(Object.keys(tinderUser)); this gives all keys of objects and store it in format of array 
+// console.log(Object.values(tinderUser)); this gives all values of objects and store it in format of array 
+// console.log(Object.entries(tinderUser)); rarely used 
 
 // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
+
+//DESTRUCTURE OF OBJECTS 
 const course = {
     coursename: "js in hindi",
     price: "999",
@@ -63,11 +67,14 @@ const course = {
 
 // course.courseInstructor
 
+ //const {key: alias}=objectname this is the better way of shorthand notation
+//this process is known as destructuring for shortening the key names
 const {courseInstructor: instructor} = course
 
 // console.log(courseInstructor);
 console.log(instructor);
 
+//this is json object
 // {
 //     "name": "hitesh",
 //     "coursename": "js in hindi",
