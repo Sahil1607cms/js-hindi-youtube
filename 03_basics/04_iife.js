02acdcd7b14a291c12d2134461d1f2cc827cea90
Sahil1607cms <149762467+Sahil1607cms@users.noreply.github.com>
2024-12-18 21:01:26 +0530

    Update 04_iife.js

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,11 +1,13 @@
 // Immediately Invoked Function Expressions (IIFE)
-
+//why is it used ? when file has database immediately it should be executed
+//also used for preventing global scope pollution
 
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
-})();
+})(); //semicolon becomes necessary for iife
 
+//syntax (function definition)()
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
 } )('hitesh')
